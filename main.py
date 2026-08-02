3db07609a75796184c9475e7aacec4074a7b7237
Seongho Yun <tgv5612@gmail.com>
2026-08-02 22:07:29 +0900

    fix: snapshot_download 함수에 repo_type 인자 추가

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from huggingface_hub import snapshot_download
 # 1. 허깅페이스에서 데이터셋전체 다운로드
 snapshot_download(
     repo_id="SKN34/SKN34-2nd-3Team", 
+    repo_type="dataset",
     local_dir="db/data"
 ) 
 
